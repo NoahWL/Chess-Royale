fd12aa790e78d082aa975e82b26c0553bb6e2eff
SirWashnerQuant <36975465+SirWashnerQuant@users.noreply.github.com>
2018-06-16 21:39:54 -0400

    Added basic gun
    Incomplete

--- RoseRoyale/Gun.py
+++ RoseRoyale/Gun.py
@@ -1,8 +1,18 @@
-
+import pygame
 
 class Gun:
-    def __init__(self):
-        pass
+    def __init__(self, posx, posy, win, terrain):
+        self.posx = posx
+        self.posy = posy
+        
+        self.win = win
+        self.pistol = pygame.image.load("pistol.png")
+        win.blit(self.pistol, (self.posx, self.posy))
+        
+    def drawGun(self, x, y):
+        self.posx = x
+        self.posy = y
+        self.win.blit(self.pistol, (x, y))
     
     def shoot(self):
         pass
