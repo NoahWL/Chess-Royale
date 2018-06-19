203cb1bae8929af3bcea9bfcbf54529efde8b241
SirWashnerQuant <36975465+SirWashnerQuant@users.noreply.github.com>
2018-06-19 09:22:51 -0400

    Finished shooting
    Merged Files

--- RoseRoyale/Graphics.py
+++ RoseRoyale/Graphics.py
@@ -1,11 +1,13 @@
 import pygame
-
+import time
 import sys
 
 from RoseRoyale.Player import Player
 from RoseRoyale.MPPlayer import MPPlayer
 from RoseRoyale.Gun import Gun
 from RoseRoyale.Bullet import Bullet
+from pygame.constants import K_a, K_d, K_SPACE, K_t
+
 
 global players
 players = []
@@ -39,6 +41,9 @@ def init():
     posx = 0
     posy = 0
     
+    lastShot =  0
+    bullets = [] 
+    
     # main loop
     while running:
         if posx > 0:
@@ -58,27 +63,36 @@ def init():
             if event.type == pygame.QUIT:
                 running = False
                 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    posx = -2
-                    
-                if event.key == pygame.K_d:
-                    posx = 2
-                    
-                if event.key == pygame.K_SPACE and player.onGround:
-                    posy = -32
+            keys = pygame.key.get_pressed()
                 
-                if event.key == pygame.K_t:
-                    pass
+            if keys[K_a]:
+                posx = -2
+                
+            if keys[K_d]:
+                posx = 2
+                
+            if keys[K_SPACE] and player.onGround:
+                posy = -32
+            
+            if keys[K_t]:
+                if time.time() - lastShot > 0.75: # How often the player can shoot in seconds
+                    bullets.append(pistol.shoot())
+                    lastShot = time.time()
+
+
                          
         if (posx != 0 or posy != 0):
             win.blit(tempBack, (0, 0))
             player.move(posx, posy, terrain)
             pistol.drawGun(player.posx + 51, player.posy + 10)
-            pistol.shoot()
             
         for mpplayer in players:
             mpplayer.draw()
+            
+        for bullet in bullets:
+            if not bullet.drawBullet():
+                bullets.remove(bullet)
+
             
         pygame.display.update()
         clock.tick(60)
