8380932e6791bf3a28271ba7a4c9610d5dd01afd
adamsnr1 <36975465+SirWashnerQuant@users.noreply.github.com>
2018-06-24 14:21:29 -0400

    Added more end screen stuff

--- RoseRoyale/EndScreen.py
+++ RoseRoyale/EndScreen.py
@@ -3,15 +3,35 @@ import pygame
 class Win:
     
     def  __init__(self):
-        pass
+        self.winScreen = pygame.image.load('winScreen.jpg').convert_alpha()
+        self.buttonMain = pygame.image.load('buttonMain.png').convert_alpha()
+        self.buttonQuit = pygame.image.load('buttonQuit.png').convert_alpha()
+        
+        
     def draw(self, win):
+        self.checkClick = True
+        self.win = win
+        win.blit(self.winScreen, (675, 350))
+        win.blit(self.buttonMain, (500, 800))
+        win.blit(self.buttonQuit, (1150, 800)) 
+        self.quitBox = pygame.Rect(1150, 800, 300, 100)
+        
+    def checkClick(self, win):
         pass
+            
+                        
+        
 class Lose:
     
     def __init__(self):
-        pass
+        self.deathScreen = pygame.image.load('deathScreen.jpeg').convert_alpha()
+        self.buttonMain = pygame.image.load('buttonMain.png').convert_alpha()
+        self.buttonQuit = pygame.image.load('buttonQuit.png').convert_alpha()
+        
     def draw(self, win):
-        pass
+        win.blit(self.deathScreen, (675, 350))
+        win.blit(self.buttonMain, (500, 800))
+        win.blit(self.buttonQuit, (1150, 800))
     
         
         
